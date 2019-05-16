6e266c44fd37614b00b02275a7b13fb263400140
hugemerge <hugemerge@gmail.com>
2019-05-16 16:32:14 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Test</h2>
+    <ul>
+      <li v-for="item in items" :key="item.sys.id">{{ item.fields.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { fetchEntries } from "@/api";
+
+export default {
+  async asyncData() {
+    return await fetchEntries('post')
+  }
+}
+</script>
