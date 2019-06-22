8e00cbe7cf0107888299b43697dabea60f7b6f06
hugemerge <hugemerge@gmail.com>
2019-06-22 14:54:59 +0900

    Add a navigation bar

---
+++ components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="navbar is-spaced">
+    <div class="navbar-brand">
+      <nuxt-link class="navbar-item" to="/">
+        <logo />
+        <span class="subtitle">sushidesu.com</span>
+      </nuxt-link>
+    </div>
+    <div class="navbar-menu"></div>
+  </nav>
+</template>
+
+
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
