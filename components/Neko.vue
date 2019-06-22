4372b8cdda85eccec7c16efcd159e92fc773273d
hugemerge <hugemerge@gmail.com>
2019-06-22 14:52:11 +0900

    Add a main component

---
+++ components/Neko.vue
@@ -0,0 +1,20 @@
+<template>
+  <canvas width="320" height="320" class='neko-canvas'></canvas>
+</template>
+
+<style scoped>
+.neko-canvas {
+  cursor: move;
+}
+</style>
+
+
+<script>
+import { makeNeko } from '~/zdog/neko.js'
+
+export default {
+  mounted() {
+    makeNeko('.neko-canvas', 1)
+}
+}
+</script>
