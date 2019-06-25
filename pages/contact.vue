863cdd82be522cbda4deb7c2a53025966b2caa70
sushidesu <45958851+sushi-desu@users.noreply.github.com>
2019-06-25 17:47:46 +0900

    Update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -5,7 +5,7 @@
         <div class="content">
           <h2 class="title">Contact</h2>
           <p>mail: sushidesu.com@gmail.com</p>
-          <p>twitter: @_sushidesu</p>
+          <p>twitter: @<a href="https://twitter.com/_sushidesu">_sushidesu</a></p>
           <p>メール または twitterのDM にてご連絡ください。</p>
         </div>
       </div>
