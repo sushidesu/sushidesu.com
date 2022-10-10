cc2c040fa07e0cd3524b44fffcdd052700239540
sushidesu <hugemerge@gmail.com>
2022-10-10 18:26:31 +0900

    :duck: add MenuItem

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,81 +1,20 @@
 <template>
   <div class="level is-mobile">
-    <div class="level-item has-text-centered">
-      <NuxtLink to="/" class="item button is-primary is-outlined">
-        <MenuNeko />
-        <span
-          class="lnk"
-          >Home</span
-        >
-      </NuxtLink>
-    </div>
-    <div class="level-item">
-      <NuxtLink to="/about" class="item button is-primary is-outlined">
-        <MenuKuma />
-        <span
-          class="lnk"
-          >About</span
-        >
-      </NuxtLink>
-    </div>
-
-    <div class="level-item">
-      <NuxtLink to="/works" class="item button is-primary is-outlined">
-        <MenuUsagi />
-        <span
-          class="lnk"
-          >Works</span
-        >
-      </NuxtLink>
-    </div>
-    <div class="level-item">
-      <NuxtLink to="/contact" class="item button is-primary is-outlined">
-        <MenuSakana />
-        <span
-          class="lnk"
-          >Contact</span
-        >
-      </NuxtLink>
-    </div>
+    <MenuItem
+      text="Home"
+      path="/"
+    ><MenuNeko /></MenuItem>
+    <MenuItem
+      text="About"
+      path="/about"
+    ><MenuKuma /></MenuItem>
+    <MenuItem
+      text="Works"
+      path="/works"
+    ><MenuUsagi /></MenuItem>
+    <MenuItem
+      text="Contact"
+      path="/contact"
+    ><MenuSakana /></MenuItem>
   </div>
 </template>
-
-<style css scoped>
-.item {
-  height: 60px;
-  border-radius: 12px;
-  width: 100%;
-}
-
-.lnk {
-  font-weight: bold;
-  font-size: 0.6rem;
-}
-
-.icon {
-  width: 40px;
-  height: 40px;
-}
-@media all and (max-width: 768px) {
-  .level-item .item.button {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .level.is-mobile .level-item:not(:last-child) {
-    margin-right: 0.2rem;
-  }
-
-  .menu-bar {
-    padding: 10px 4px;
-  }
-}
-
-@media all and (min-width: 769px) {
-  .lnk {
-    font-size: 1rem;
-  }
-}
-</style>
---
+++ src/components/MenuItem.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="level-item has-text-centered">
+    <NuxtLink :to="props.path" class="item button is-primary is-outlined">
+      <slot />
+      <span
+        class="lnk"
+        >{{ props.text }}</span
+      >
+    </NuxtLink>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  text: string
+  path: string
+}>()
+</script>
+
+<style css scoped>
+.item {
+  height: 60px;
+  border-radius: 12px;
+  width: 100%;
+}
+
+.lnk {
+  font-weight: bold;
+  font-size: 0.6rem;
+}
+
+.icon {
+  width: 40px;
+  height: 40px;
+}
+@media all and (max-width: 768px) {
+  .level-item .item.button {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .level.is-mobile .level-item:not(:last-child) {
+    margin-right: 0.2rem;
+  }
+
+  .menu-bar {
+    padding: 10px 4px;
+  }
+}
+
+@media all and (min-width: 769px) {
+  .lnk {
+    font-size: 1rem;
+  }
+}
+</style>
