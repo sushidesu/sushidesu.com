cf47849a7607431aef91e00ca0237ec1a6bf0759
sushidesu <hugemerge@gmail.com>
2022-10-10 15:57:32 +0900

    :shower: fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,9 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true
-  },
   "extends": [
-    "plugin:vue/base",
-    "plugin:vue/recommended",
-    "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "@nuxtjs/eslint-config-typescript",
+    "plugin:vue/vue3-recommended",
     "prettier"
-  ],
-  "parser": "vue-eslint-parser",
-  "parserOptions": {
-    "ecmaVersion": 12,
-    "parser": "@typescript-eslint/parser",
-    "sourceType": "module"
-  },
-  "plugins": [
-    "vue",
-    "@typescript-eslint"
-  ],
-  "rules": {
-  }
+  ]
 };
